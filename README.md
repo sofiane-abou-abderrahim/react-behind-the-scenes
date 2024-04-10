6c4327cce7b239fefce372148b579a8cf1cd09cc
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2024-04-10 14:46:50 +0100

    1. Avoiding Component Function Executions with memo()

--- README.md
+++ README.md
@@ -14,3 +14,11 @@
 1. run `npm install`
 2. run `npm run dev`
 3. create `README.md`
+
+## 1. Avoiding Component Function Executions with memo()
+
+1. wrap the `Counter.jsx` component function with the `memo` function imported from React
+   => `memo` only prevents function executions that are triggered by the parent component, so in this case the `App.jsx` component
+   => `memo` does not care about internal changes
+   => But external changes of course only makes sense for this component here to be executed if the prop value changed
+2. Don't overuse `memo` because checking props with `memo` costs performance
--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { memo, useState } from 'react';
 
 import IconButton from '../UI/IconButton.jsx';
 import MinusIcon from '../UI/Icons/MinusIcon.jsx';
@@ -7,11 +7,7 @@ import CounterOutput from './CounterOutput.jsx';
 import { log } from '../../log.js';
 
 function isPrime(number) {
-  log(
-    'Calculating if is prime number',
-    2,
-    'other'
-  );
+  log('Calculating if is prime number', 2, 'other');
   if (number <= 1) {
     return false;
   }
@@ -27,18 +23,18 @@ function isPrime(number) {
   return true;
 }
 
-export default function Counter({ initialCount }) {
+const Counter = memo(function Counter({ initialCount }) {
   log('<Counter /> rendered', 1);
   const initialCountIsPrime = isPrime(initialCount);
 
   const [counter, setCounter] = useState(initialCount);
 
   function handleDecrement() {
-    setCounter((prevCounter) => prevCounter - 1);
+    setCounter(prevCounter => prevCounter - 1);
   }
 
   function handleIncrement() {
-    setCounter((prevCounter) => prevCounter + 1);
+    setCounter(prevCounter => prevCounter + 1);
   }
 
   return (
@@ -58,4 +54,6 @@ export default function Counter({ initialCount }) {
       </p>
     </section>
   );
-}
+});
+
+export default Counter;
